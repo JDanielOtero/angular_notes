6105022f2fb2e2dd7311c2a6595293a7a7aca7a9
Daniel Otero <jdanielotero01@gmail.com>
2025-04-02 22:09:07 -0500

    basic types

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,10 @@
+const name: string = 'Dotero';
+let hpPoints: number | string = 95 ;
+const isAlive: boolean = true;
+
+hpPoints = 'FULL';
+console.log({
+    name,hpPoints,isAlive
+});
+
+export{};
