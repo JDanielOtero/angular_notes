35ae6621525ec1f62bb488aba17c4fc924c020a5
Daniel Otero <jdanielotero01@gmail.com>
2025-04-10 22:09:39 -0500

    comentarios

--- 01-typescript-intro/src/topics/06-function-destructuring.ts
+++ 01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -1,4 +1,7 @@
 // Definición de la interfaz Product
+// Cada producto debe tener:
+// - `description`: una cadena que describe el producto.
+// - `price`: un número que indica el precio del producto.
 interface Product {
     description: string;
     price: number;
@@ -16,6 +19,11 @@ const tablet: Product = {
 };
 
 // Definición de la interfaz para las opciones de cálculo de impuestos
+// Esta interfaz `TaxCalculationOptions` define los parámetros que necesita la función de cálculo de impuestos.
+// Incluye:
+// - `tax`: un número que representa el porcentaje de impuesto (por ejemplo 0.15 para 15%).
+// - `products`: un arreglo de objetos tipo `Product`, es decir, los productos sobre los que se calculará el impuesto.
+
 interface TaxCalculationOptions {
     tax: number;
     products: Product[];
@@ -30,13 +38,17 @@ function taxCalculation(options: TaxCalculationOptions): number[] {
     return [total,total*options.tax]
 }
 
+
 const shoppingCart = [phone, tablet];
 const tax = 0.15;
 
+// Llamamos a la función `taxCalculation` pasándole los productos y el impuesto.
+// Como usamos la forma corta `tax`, esto es equivalente a `tax: tax`.
 const result = taxCalculation({
     products: shoppingCart,
-    tax,         // tax:tax,
-  })
+    tax,
+});
+
 
 console.log('Total',result[0]);
 console.log('Total',result[0]);
