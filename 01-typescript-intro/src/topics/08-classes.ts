d3ef9a969adf60148505a547e70950ff5542a899
Daniel Otero <jdanielotero01@gmail.com>
2025-05-03 11:44:35 -0500

    Classes second

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -1,35 +1,54 @@
-export class Person {
-    // public name: string;
-    // private address: string;
 
-    // constructor( name: string, address: string) {
-    //     this.name = name;
-    //     this.address = address;
+    export class Person {
+        // public name: string;
+        // private address: string;
+
+        // constructor( name: string, address: string) {
+        //     this.name = name;
+        //     this.address = address;
+        // }
+
+        // Una fomra mas comun y corta de declarar el constructor en typescript
+
+        constructor(
+            public name:string,
+            private address: string = 'No Address'
+        ){}
+    }
+
+    const ironman1 = new Person('Ironman','New York');
+    console.log(ironman1)
+
+    // // Las propiedades se extienden
+    // export class Hero extends Person{
+
+    //     constructor(
+    //         public alterEgo: string,
+    //         public age: number,
+    //         public realName: string,
+    //     ){
+    //         super(realName , 'New York');
+    //         // Super es de la clase que yo me extiendo 
+    //     }
     // }
 
-    // Una fomra mas comun y corta de declarar el constructor en typescript
+    // OTRA FORMA DE HACELO, LA CLASE HERO SERA AFECTADA POR SI LA CALSE PERSONA CAMBIA 
+    export class Hero {
 
-    constructor(
-        public name:string,
-        private address: string = 'No Address'
-    ){}
-}
+        // public person: Person;
 
-const ironman1 = new Person('Ironman','New York');
-console.log(ironman1)
-
-// Las propiedades se extienden
-export class Hero extends Person{
-
-    constructor(
-        public alterEgo: string,
-        public age: number,
-        public realName: string,
-    ){
-        super(realName , 'New York');
-        // Super es de la clase que yo me extiendo 
+        constructor(
+            public alterEgo: string,
+            public age: number,
+            public realName: string,
+            public person: Person,
+        ){
+            // this.person = new Person(realName)
+        }
     }
-}
 
-const ironman = new Hero('Ironman',45,'Tony');
-console.log(ironman)
+
+    const tony = new Person('Tony Startk', 'New York')
+    const ironman = new Hero('Ironman',45,'Tony',tony);
+    console.log(ironman)
+
