87332589ee329af1651c7ff16eee1f89817e43f7
Daniel Otero <jdanielotero01@gmail.com>
2025-05-02 22:26:35 -0500

    Import Example

---
+++ 01-typescript-intro/src/topics/07-import-export.ts
@@ -0,0 +1,13 @@
+
+import { Product } from './06-function-destructuring' ;
+
+const shopingCart: Product[] = [
+    {
+        description: 'Nokia' ,
+        price: 100
+    },
+    {
+        description: 'iPad' ,
+        price: 150
+    }
+];
