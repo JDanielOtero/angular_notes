a73cbe5b89a050171725088d31f0eb6a29a2210d
Daniel Otero <jdanielotero01@gmail.com>
2025-05-03 11:30:47 -0500

    Classes

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -12,10 +12,24 @@ export class Person {
     constructor(
         public name:string,
         private address: string = 'No Address'
-    ){
+    ){}
+}
 
+const ironman1 = new Person('Ironman','New York');
+console.log(ironman1)
+
+// Las propiedades se extienden
+export class Hero extends Person{
+
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+    ){
+        super(realName , 'New York');
+        // Super es de la clase que yo me extiendo 
     }
 }
 
-const ironman = new Person('Ironman','New York');
+const ironman = new Hero('Ironman',45,'Tony');
 console.log(ironman)
